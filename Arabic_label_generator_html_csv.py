53ea7edba0f3a2253199b9c0961c84bfe50f8b57
shuiyiwensi <wtan4210@uni.sydney.edu.au>
2018-08-26 01:28:18 +1000

    commit all

--- Arabic_label_generator_html_csv.py
+++ Arabic_label_generator_html_csv.py
@@ -244,6 +244,5 @@ with open('input_excel/input.csv', 'r',encoding="utf-16") as csvfile:
         print(expire_time)
         print(Chinese_ProductPlace)
         rownumber=get_row_number(int(row[6]),Chinese_name_to_row_number_dictionary)
-
         write2html(4,5,rownumber, Chinese_Name, Ingredients_name_list, product_weight_number,
                    product_weight, product_time, expire_time, Chinese_ProductPlace)
